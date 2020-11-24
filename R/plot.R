75c32329e8492c34b4d0b3f8a9b670eac0b136e7
Seb Dalgarno <seb.dalgarno@gmail.com>
2020-11-23 19:37:51 -0800

    add plot_growth fun

---
+++ R/plot.R
@@ -0,0 +1,15 @@
+#' Plot kitten growth
+#'
+#' @param kittens A data.frame of the kitten growth.
+#' @return A ggplot2 object
+#' @export
+#' @examples
+#' plot_growth()
+plot_growth <- function(kittens = purrfectdata::kitten_growth){
+  ggplot2::ggplot(data = kittens,
+                  ggplot2::aes_string(x = "datetime", y = "mass", group = "name", color = "name")) +
+    ggplot2::geom_point(size = 1) +
+    ggplot2::geom_line() +
+    ggplot2::labs(x = "Date", y = "Mass") +
+    ggplot2::scale_color_discrete(name = "Kitten")
+}
